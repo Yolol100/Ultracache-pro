a237bbac9a78a3833301cfa43b8fd3f43e634bb2
Yolol100 <Andrew_Baeten@hotmail.com>
2026-05-29 03:12:37 +0700

    Gate CSS delivery behind Testing Mode

--- includes/css/class-ucp-css.php
+++ includes/css/class-ucp-css.php
@@ -10,6 +10,11 @@ class UCP_CSS {
     use UCP_CSS_Artifact_Trait;
 
     public function __construct() {
+        $frontend_context = !is_admin() && !(function_exists('wp_doing_cron') && wp_doing_cron()) && !(defined('WP_CLI') && WP_CLI);
+        if ($frontend_context && !UCP_Helpers::frontend_optimizations_allowed()) {
+            return;
+        }
+
         add_filter('ucp_process_html', array($this, 'process_css'), 20);
     }
 }
